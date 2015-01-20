fe2063802f647343e5842b9a9dae8c693bb65b8b
Zack Hoffmann <zachary.hoffmann@gmail.com>
2015-01-19 19:46:05 -0500

    Starting on session registration (first step in auth).

--- src/main/java/org/thehoffmanns/letschat/server/ChatMediator.java
+++ src/main/java/org/thehoffmanns/letschat/server/ChatMediator.java
@@ -1,6 +1,16 @@
 package org.thehoffmanns.letschat.server;
 
-public interface ChatMediator {
+public class ChatMediator {
     
+    private static final ChatMediator i = new ChatMediator();
     
+    public static ChatMediator instance() {
+        return i;
+    }
+    
+    private ChatMediator() {
+        
+    }
+    
+    // TODO register UUID
 }
--- src/main/java/org/thehoffmanns/letschat/server/ChatSocket.java
+++ src/main/java/org/thehoffmanns/letschat/server/ChatSocket.java
@@ -1,6 +1,9 @@
 package org.thehoffmanns.letschat.server;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.jetty.websocket.api.Session;
@@ -15,35 +18,36 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
  */
 @WebSocket
 public class ChatSocket {
-    
-    private Session ses;
-    
+
+    private static final Map<Session, UUID> SESSION_TO_ID = new HashMap<>();
+    private static final Map<UUID, Session> ID_TO_SESSION = new HashMap<>();
+
     @OnWebSocketConnect
     public void onConnect(Session ses) {
-        // TODO test session hashcode and map that?
-        this.ses = ses;
+        if (!SESSION_TO_ID.keySet().contains(ses)) {
+            SESSION_TO_ID.put(ses, UUID.randomUUID());
+            ID_TO_SESSION.put(SESSION_TO_ID.get(ses), ses);
+        }
     }
-    
+
     @OnWebSocketClose
     public void onClose(Session ses, int code, String reason) {
-        
+        ID_TO_SESSION.remove(SESSION_TO_ID.get(ses));
+        SESSION_TO_ID.remove(ses);
     }
-    
+
     @OnWebSocketMessage
     public void onMessage(Session ses, String msg) {
         try {
-            
+
             Logger.getLogger(ChatSocket.class.getName()).log(Level.FINE, msg);
             // TODO Gson msg into Message object
-            if (this.ses.equals(ses)) {
-                ses.getRemote().sendString(msg);
-            } else {
-                ses.getRemote().sendString("NOT EQUAL!!!");
-            }
-            
+
+            ses.getRemote().sendString(msg);
+
         } catch (IOException ex) {
             Logger.getLogger(ChatSocket.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
